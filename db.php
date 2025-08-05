fe56ee914c7c903cbc2089bf55800c313a30b0e3
Marco_Tomasini <marco_tomasini@estudante.sesisenai.org.br>
2025-08-05 09:30:30 -0300

    ADD - inicio do sitema crud

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "crud_system_marco";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("conexão falhou: " . $conn->connect_error);
+    }
+
+?>
---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php
+
+    include 'db.php';
+    $id = $_GET['id'];
+
+    $sql = "DELETE FROM usuario WHERE id=$id";
+
+    if($conn -> query($sql) === true){
+        echo "Registro excluido com sucesso!";
+    }else{
+        echo"Erro " . $sql . "<br>" . $conn->error;
+    }
+    $conn -> close();
+
+    header("Location: read.php");
+?>
